4658af8d9b93ba9c9ccf80dcffea8e3a4422ba9d
fabioklatt <fabio-fonseca@live.com>
2022-06-02 22:25:41 -0300

    finalizada

--- CURSO EM VIDEO/calculadora/index.js
+++ CURSO EM VIDEO/calculadora/index.js
@@ -1,10 +1,30 @@
 
-function num(){
-    
-    
+const result = document.querySelector('.result')
+const igual = document.querySelector('.igual')
+
+
+function insert(valor){
+  result.innerHTML += valor
+}
+
+function clean(){
+  const ce = document.querySelector('.butsce')
+  result.innerHTML=''
+}
+
+function backspace (){
+  if (result.innerHTML){
+    let res = result.innerHTML
+    result.innerHTML = res.substring(0,res.length-1)
   }
+}
 
-
+function finalres (){
+  
+  let r = eval(result.innerHTML)
+  result.innerHTML = r
+  
+}
 
 
 
--- CURSO EM VIDEO/repetições/index.js
+++ CURSO EM VIDEO/repetições/index.js
@@ -1,17 +1,6 @@
-/*
-let x = 1
 
-while (x <= 5){
-    y = 5*x
-    console.log(`5 x ${x} = ${y}`)
 
-    x++
-}
-*/
 
-for (x=1; x<=10; x++){
-    y = 5*x
-    console.log(`5 x ${x} = ${y} `)
-}
+
 
 
